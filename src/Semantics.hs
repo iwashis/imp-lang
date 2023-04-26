796be720278cec69c7e82c42071c64daf030a3c9
Tomasz Brengos <tomasz@genlambda.com>
2023-04-26 14:15:15 +0200

    Add complete small step semantics to IMP langauge

--- src/Semantics.hs
+++ src/Semantics.hs
@@ -21,7 +21,7 @@ type Store = Map VariableName Value
 -- small step semantics for Arithmetic expressions present
 -- in our language:
 stepAr :: (Expr Arithmetic, Store) -> Maybe (Expr Arithmetic, Store)
-stepAr (Constant n, s) = Nothing
+stepAr (Constant _, _) = Nothing
 stepAr (Var x, s) = do
     n <- Map.lookup x s
     pure (Constant n ,s)
@@ -33,7 +33,42 @@ stepAr (Op2 o e1 e2, s) = case e1 of
         (e1',_) <- stepAr (e1, s)
         pure  (Op2 o e1' e2, s)
 
+toExpr :: Bool -> Expr Bool
+toExpr True = T
+toExpr False = F
+
+stepBool :: ( Expr Bool , Store ) -> Maybe ( Expr Bool , Store )
+stepBool (T, _) = Nothing
+stepBool (F, _) = Nothing
+stepBool (LessOrEq e1 e2,s) = case (e1,e2) of
+    (Constant m, Constant n) -> pure (toExpr (m <= n), s)
+    (Constant m, _) -> do
+        (e2',_) <- stepAr (e2, s)
+        pure (LessOrEq (Constant m) e2', s)
+    _               -> do
+        (e1',_) <- stepAr (e1, s)
+        pure (LessOrEq e1' e2, s)
+
+stepCommand :: (Expr Comm, Store ) -> Maybe (Expr Comm, Store)
+stepCommand (Skip, _) = Nothing
+stepCommand (Assign x e, s) = case e of
+    Constant n -> pure (Skip, Map.insert x n s )
+    _          -> do
+        (e',s') <- stepAr (e, s)
+        pure (Assign x e', s') 
+stepCommand (AndThen e1 e2, s) = case e1 of
+    Skip  -> pure (e2,s)
+    _     -> do
+        (e1',s') <- stepCommand ( e1, s )
+        pure (AndThen e1' e2, s')
+stepCommand (IfElse b e1 e2, s) = case b of
+    T -> pure (e1, s)
+    F -> pure (e2, s)
+    _ -> do
+        (b',s') <- stepBool (b, s)
+        pure (IfElse b' e1 e2, s')
+stepCommand (While b e, s) = 
+    pure (IfElse b (AndThen e (While b e)) Skip,s)
 
 -- TODO list : 
--- 1. complete small-step semantics
 -- 2. write big-step semantics and compare the two approaches
