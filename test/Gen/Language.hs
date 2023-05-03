b30a1591b3cfc115bc449bb66e911835fb529ce7
Tomasz Brengos <tomasz@genlambda.com>
2023-05-03 21:35:02 +0200

    Finish gen for Expr a with type coercion

--- test/Gen/Language.hs
+++ test/Gen/Language.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+
 module Gen.Language where
 
 import Language
 
 import Test.QuickCheck
-
+import Data.Coerce
+import Data.Typeable hiding (TypeRep)
 
 instance Arbitrary BinOp where
   arbitrary = elements [Add, Sub, Mul, Div]
@@ -33,21 +38,30 @@ genCommExpr = oneof
     , IfElse <$> genBoolExpr <*> genCommExpr <*> genCommExpr -- generate an if-else expression with a boolean expression and two sub-expressions
     , While <$> genBoolExpr <*> genCommExpr -- generate a while loop expression with a boolean expression and a sub-expression
     ]
-    {-
--- Define a generator for all expressions
-genExpr :: Gen (Expr a)
-genExpr = oneof 
-    [ coerce (Var <$> arbitrary)
-    , coerce (Constant <$> arbitrary)
-    , coerce (Op2 <$> arbitrary <*> genArithmeticExpr <*> genArithmeticExpr)
-    , coerce $ pure T
-    , coerce $ pure F
-    , coerce $ LessOrEq <$> genArithmeticExpr <*> genArithmeticExpr
-    , coerce $ pure Skip
-    , coerce $ Assign <$> arbitrary <*> genArithmeticExpr
-    , coerce $ AndThen <$> genExpr <*> genExpr
-    , coerce $ IfElse <$> genBoolExpr <*> genExpr <*> genExpr
-    , coerce $ While <$> genBoolExpr <*> genExpr
-    ]
 
--}
+
+-- Type representative
+data MyTypeRep a where
+  TypeArithmetic :: MyTypeRep Arithmetic
+  TypeBool       :: MyTypeRep Bool
+  TypeComm       :: MyTypeRep Comm
+
+-- Function to obtain MyTypeRep for Typeable types
+myTypeRep :: forall a. Typeable a => Proxy a -> MyTypeRep a
+myTypeRep _ = case eqT :: Maybe (a :~: Arithmetic) of
+    Just Refl -> TypeArithmetic
+    Nothing   -> case eqT :: Maybe (a :~: Bool) of
+        Just Refl -> TypeBool
+        Nothing   -> case eqT :: Maybe (a :~: Comm) of
+            Just Refl -> TypeComm
+            Nothing   -> error "Invalid type"
+
+-- Define a generator for Expr a
+genExpr :: forall a. (Typeable a, Arbitrary a) => Gen (Expr a)
+genExpr = genExprWithRep (myTypeRep (Proxy :: Proxy a))
+
+-- Helper function to generate Expr a based on the type representative
+genExprWithRep :: MyTypeRep a -> Gen (Expr a)
+genExprWithRep TypeArithmetic = genArithmeticExpr
+genExprWithRep TypeBool       = genBoolExpr
+genExprWithRep TypeComm       = genCommExpr
